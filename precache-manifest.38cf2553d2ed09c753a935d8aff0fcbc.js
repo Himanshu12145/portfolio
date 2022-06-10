8fd9f225290d93ccd84392afe96ad0cdc66d4744
HIMANSHU SAHOO <himansu12145@gmail.com>
2022-06-10 12:04:43 +0530

    Updates

---
+++ precache-manifest.38cf2553d2ed09c753a935d8aff0fcbc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "448430c2f710940f74df98d4f1a818c0",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a88f4d33c324148b2d36",
+    "url": "/portfolio/static/css/main.d84fe515.chunk.css"
+  },
+  {
+    "revision": "bd4abeff626c3e6b0a2d",
+    "url": "/portfolio/static/js/2.3440b1d0.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/portfolio/static/js/2.3440b1d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a88f4d33c324148b2d36",
+    "url": "/portfolio/static/js/main.8d641a47.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "ce0ff373298a0688f4d6eb8c71202c2e",
+    "url": "/portfolio/static/media/photoO.ce0ff373.jpeg"
+  }
+]);
